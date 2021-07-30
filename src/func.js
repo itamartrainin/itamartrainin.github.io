3ce9e74811ae8c5c22ee4128593a7de3720817bd
Itamar Trainin <tamtimart@gmail.com>
2021-07-30 16:51:59 +0300

    second

--- src/func.js
+++ src/func.js
@@ -1,34 +1,9 @@
-// function alignTextboxToText(textElem, boxElem) {
-//     text_rect = textElem.getBoundingClientRect()
-//     box_rect = boxElem.getBoundingClientRect()
-//     dx = text_rect.left - box_rect.left
-//     dy = text_rect.top - box_rect.top
-//     boxElem.style.left = dx 
-//     boxElem.style.top = dy   
-// }
+function scrollToAnchor(anchor) {
+    document.querySelector(anchor).scrollIntoView({
+        behavior: 'smooth'
+    });
+}
 
-// alignTextboxToText(document.getElementById('about_me'), document.getElementById('text_box_1'))
-
-// function getScrollPercent() {
-//     var h = document.documentElement, 
-//         b = document.body,
-//         st = 'scrollTop',
-//         sh = 'scrollHeight';
-//     return (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100;
-// }
-
-// function runScrollEvents() {
-//     slideIn(document.getElementById('text_box_1_0'))
-// }
-
-// function isInViewport(el) {
-//     const rect = el.getBoundingClientRect();
-//     const ret = rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) 
-//     return ret
-// }
-
-// function slideIn(el) {
-//     console.log(getScrollPercent())
-// }
-
-// window.addEventListener("scroll", runScrollEvents);
+function openInNewTab(url) {
+    window.open(url, '_blank').focus();
+}
